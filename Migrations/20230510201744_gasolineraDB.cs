6dcacd1fc4a24e6ed405b151c944620656cde0d0
Andynajera <andrenaje94@hotmail.com>
2023-05-10 22:29:45 +0200

    ok

---
+++ Migrations/20230510201744_gasolineraDB.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto2EV.Migrations
+{
+    public partial class gasolineraDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TipoGasoleos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    number = table.Column<int>(type: "int", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    contamina = table.Column<bool>(type: "bit", nullable: false),
+                    date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoGasoleos", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    gender = table.Column<bool>(type: "bit", nullable: false),
+                    dateInscription = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    birth = table.Column<int>(type: "int", nullable: false),
+                    puntosAcumulados = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Promociones",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    vigor = table.Column<bool>(type: "bit", nullable: false),
+                    date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    descuento = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    cantidadPersonas = table.Column<int>(type: "int", nullable: false),
+                    Userid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Promociones", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Promociones_User_Userid",
+                        column: x => x.Userid,
+                        principalTable: "User",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Promociones_Userid",
+                table: "Promociones",
+                column: "Userid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Promociones");
+
+            migrationBuilder.DropTable(
+                name: "TipoGasoleos");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
